fec8056f7bc1ab241623243f0d0988ff48301434
unnati1028 <62767100+unnati1028@users.noreply.github.com>
2022-06-07 10:48:34 -0500

    works yay

--- src/main/java/backend/models/Employee.java
+++ src/main/java/backend/models/Employee.java
@@ -1,14 +1,14 @@
 package backend.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.springframework.data.annotation.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/backend/services/EmployeeService.java
+++ src/main/java/backend/services/EmployeeService.java
@@ -3,14 +3,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import backend.models.Employee;
 import backend.repositories.EmployeeRepository;
-
+@CrossOrigin("*")
 @Service
+@RestController
+@RequestMapping("/api")
 public class EmployeeService {
 	
 	@Autowired
